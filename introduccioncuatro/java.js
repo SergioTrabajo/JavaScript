9f8a7f8e531261c7c48e3b6a976c78c0b9faf9b8
Sergio Gómez Herrera <118183734+SergioTrabajo@users.noreply.github.com>
2023-03-24 12:36:42 +0100

    Add files via upload

---
+++ introduccioncuatro/java.js
@@ -0,0 +1,28 @@
+function cambiaTítulo(){
+    let miTítulo = document.getElementById ( "título" );
+    
+    if (miTítulo.innerHTML == "Título de página") {
+        miTítulo.innerHTML = "Hola gente del github";
+    } else {
+        miTítulo.innerHTML = "SA MATO PAAACOOOOO";
+    } 
+}
+function cambiarAncho(){
+    let miImage = document.getElementById("paco")
+    miImage.width = "500";
+}
+function cambiarImage(){
+    let miImage = document.getElementById("paco")
+    // miImage.src = "images/paddock.jpg"
+    miImage.src = "images/paco.jpg"
+}
+function cambiarTitle(){
+    let title = document.getElementById("título");
+    title.style.color = "red";
+    title.style.fontSize = "4em";
+    title.style.fontStyle = "italic";
+}
+function desaparecerImage(){
+    let miImage = document.getElementById("paco")
+    miImage.style.display = "none"
+}
