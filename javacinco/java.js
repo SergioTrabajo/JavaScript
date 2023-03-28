515b1337422884f6f60f0e356ba8809d79c67e67
Sergio Gómez Herrera <118183734+SergioTrabajo@users.noreply.github.com>
2023-03-28 12:39:09 +0200

    Add files via upload

---
+++ javacinco/java.js
@@ -0,0 +1,12 @@
+function cambiarCapa(){
+    let miCapa = document.getElementById("capa1");
+    miCapa.innerHTML = "<h1>Texto de prueba pinches pelotudos</h1>";
+    miCapa.style.backgroundColor= "lightgreen"
+    miCapa.style.fontFamily= "Arial"
+
+}
+function cambiarImage(){
+    let miImage = document.getElementById("image");
+    miImage.src = "https://cdn.memegenerator.es/descargar/714773";
+
+}
